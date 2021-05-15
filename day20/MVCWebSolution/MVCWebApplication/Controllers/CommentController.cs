29b3fcd4735c6718aaf0917d621d22f8b3c1fbb7
Dhesh Kumaar A <dheshkumaar30@gmail.com>
2021-05-15 16:21:24 +0530

    12-05-2021

---
+++ day20/MVCWebSolution/MVCWebApplication/Controllers/CommentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApplication.Controllers
+{
+    public class CommentController : Controller
+    {
+        static List<Comment> comments = new List<Comment>()
+            {
+                new Comment() { Id= 1,CommenText="First comment",PostId=101},
+                new Comment() { Id = 2, CommenText="Second comment",PostId=102 },
+                new Comment() { Id = 3, CommenText="Third comment",PostId=103}
+            };
+        public IActionResult Index()
+        {
+            return View(comments);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Comment comment)
+        {
+            comments.Add(comment);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            int idx = comments.FindIndex(p => p.Id == id);
+            return View(comments[idx]);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Comment comment)
+        {
+            int idx = comments.FindIndex(p => p.Id == id);
+            comments[idx].CommenText = comment.CommenText;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ day20/MVCWebSolution/MVCWebApplication/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApplication.Models
+{
+    public class Comment
+    {
+        public int PostId { get; set; }
+        public int Id { get; set; }
+        public string CommenText { get; set; }
+    }
+}
